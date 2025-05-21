8c2ceb930887a51597d9e4a05b5bc485e8ca16b1
Andrea Basili <andrea.basili@live.it>
2025-05-21 15:04:30 +0200

    render pet gallery

--- src/app/profile-gallery/profile-gallery.component.ts
+++ src/app/profile-gallery/profile-gallery.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { PetService } from '../service/pet.service';
+import { Pet } from '../model/Pet';
+// import { JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'app-profile-gallery',
@@ -6,6 +9,15 @@ import { Component } from '@angular/core';
   templateUrl: './profile-gallery.component.html',
   styleUrl: './profile-gallery.component.css'
 })
-export class ProfileGalleryComponent {
+export class ProfileGalleryComponent implements OnInit {
+  petList: Pet[] = []
+  petService: PetService = inject(PetService)
 
+  ngOnInit() {
+    this.getPets()
+  }
+
+  getPets() {
+    this.petService.getPets().subscribe(pets => this.petList = pets)
+  }
 }
