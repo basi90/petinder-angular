be922648e82578b2f63535baa329afbe5deb26f4
Andrea Basili <andrea.basili@live.it>
2025-05-21 15:51:57 +0200

    sort

--- src/app/profile-gallery/profile-gallery.component.ts
+++ src/app/profile-gallery/profile-gallery.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, inject } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PetService } from '../service/pet.service';
 // import { Pet } from '../model/Pet';
