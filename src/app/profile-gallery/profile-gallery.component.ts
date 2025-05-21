8884dfb39e57367df797e8f62348caec28696b3d
Andrea Basili <andrea.basili@live.it>
2025-05-21 15:21:16 +0200

    async pipe

--- src/app/profile-gallery/profile-gallery.component.ts
+++ src/app/profile-gallery/profile-gallery.component.ts
@@ -1,23 +1,27 @@
 import { Component, OnInit, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { PetService } from '../service/pet.service';
-import { Pet } from '../model/Pet';
+// import { Pet } from '../model/Pet';
+import { AsyncPipe } from '@angular/common';
 // import { JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'app-profile-gallery',
-  imports: [],
+  imports: [AsyncPipe, CommonModule],
   templateUrl: './profile-gallery.component.html',
   styleUrl: './profile-gallery.component.css'
 })
-export class ProfileGalleryComponent implements OnInit {
-  petList: Pet[] = []
+export class ProfileGalleryComponent{
+  // petList: Pet[] = []
   petService: PetService = inject(PetService)
 
-  ngOnInit() {
-    this.getPets()
-  }
+  pets$ = this.petService.getPets()
 
-  getPets() {
-    this.petService.getPets().subscribe(pets => this.petList = pets)
-  }
+  // ngOnInit() {
+  //   this.getPets()
+  // }
+
+  // getPets() {
+  //   this.petService.getPets().subscribe(pets => this.petList = pets)
+  // }
 }
