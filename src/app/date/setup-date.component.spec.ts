1ef50fb3e4bc1744bcc8a74f1f9d6c648201d00b
Andrea Basili <andrea.basili@live.it>
2025-05-23 11:03:31 +0200

    codelab06

--- src/app/date/setup-date.component.spec.ts
+++ src/app/date/setup-date.component.spec.ts
@@ -1,16 +1,37 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { SetupDateComponent } from './setup-date.component';
+import { ActivatedRoute } from '@angular/router';
+import { of } from 'rxjs';
+import { PetService } from '../service/pet.service';
+import { FormBuilder } from '@angular/forms';
 
 describe('SetupDateComponent', () => {
   let component: SetupDateComponent;
   let fixture: ComponentFixture<SetupDateComponent>;
 
+  const mockActivatedRoute = {
+    snapshot: {
+      paramMap: {
+        get: (key: string) => 'Fluffy'  // Simulated pet name
+      }
+    }
+  };
+
+  const mockPetService = {
+    getPets: () => of([
+      { name: 'Fluffy', kind: 'Dog', image: 'fluffy.jpg', profileText: 'Loves bones', popularity: 5 }
+    ])
+  };
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [SetupDateComponent]
-    })
-    .compileComponents();
+      imports: [SetupDateComponent],
+      providers: [
+        { provide: ActivatedRoute, useValue: mockActivatedRoute },
+        { provide: PetService, useValue: mockPetService },
+        FormBuilder
+      ]
+    }).compileComponents();
 
     fixture = TestBed.createComponent(SetupDateComponent);
     component = fixture.componentInstance;
--- src/app/profile-gallery/profile-gallery.component.spec.ts
+++ src/app/profile-gallery/profile-gallery.component.spec.ts
@@ -1,23 +1,33 @@
-// import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ProfileGalleryComponent } from './profile-gallery.component';
+import { of } from 'rxjs';
+import { PetService } from '../service/pet.service';
+import { FormBuilder } from '@angular/forms';
 
-// import { ProfileGalleryComponent } from './profile-gallery.component';
+describe('ProfileGalleryComponent', () => {
+  let component: ProfileGalleryComponent;
+  let fixture: ComponentFixture<ProfileGalleryComponent>;
 
-// describe('ProfileGalleryComponent', () => {
-//   let component: ProfileGalleryComponent;
-//   let fixture: ComponentFixture<ProfileGalleryComponent>;
+  const petServiceMock = {
+    getPets: () => of([]),
+    addPet: () => of({})
+  };
 
-//   beforeEach(async () => {
-//     await TestBed.configureTestingModule({
-//       imports: [ProfileGalleryComponent]
-//     })
-//     .compileComponents();
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [ProfileGalleryComponent],
+      providers: [
+        { provide: PetService, useValue: petServiceMock },
+        FormBuilder // Also needed for form injection
+      ]
+    }).compileComponents();
 
-//     fixture = TestBed.createComponent(ProfileGalleryComponent);
-//     component = fixture.componentInstance;
-//     fixture.detectChanges();
-//   });
+    fixture = TestBed.createComponent(ProfileGalleryComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
 
-//   it('should create', () => {
-//     expect(component).toBeTruthy();
-//   });
-// });
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
