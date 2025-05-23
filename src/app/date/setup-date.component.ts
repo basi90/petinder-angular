afab6dfb1c280ce0083439f3d29c6c217748ae62
Andrea Basili <andrea.basili@live.it>
2025-05-23 10:54:39 +0200

    codelab06

--- src/app/date/setup-date.component.ts
+++ src/app/date/setup-date.component.ts
@@ -1,20 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ActivatedRoute } from '@angular/router';
-import { RouterModule } from '@angular/router';
+import { ActivatedRoute, RouterModule } from '@angular/router';
+import { PetService } from '../service/pet.service';
+import { Pet } from '../model/Pet';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-setup-date',
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, ReactiveFormsModule],
   templateUrl: './setup-date.component.html',
   styleUrl: './setup-date.component.css'
 })
 export class SetupDateComponent implements OnInit {
   petName: string = ''
+  pet: Pet | null = null
+  sendTextForm: FormGroup
 
-  constructor(private route: ActivatedRoute) {}
+  private route = inject(ActivatedRoute);
+  private petService = inject(PetService);
+  private formBuilder = inject(FormBuilder);
+
+
+  constructor() {
+      this.sendTextForm = this.formBuilder.group({
+      message: ['']
+    });
+  }
 
   ngOnInit(): void {
-      this.petName = this.route.snapshot.paramMap.get('name') ?? ''
+    this.petName = this.route.snapshot.paramMap.get('name') ?? '';
+    this.petService.getPets().subscribe((pets: Pet[]) => {
+      this.pet = pets.find(p => p.name === this.petName) ?? null;
+    });
+  }
+
+    onSubmit(): void {
+    if (this.sendTextForm.valid) {
+      const message = this.sendTextForm.value.message;
+      console.log(`Sending message to ${this.petName}: ${message}`);
+      this.sendTextForm.reset();
+    }
   }
 }
